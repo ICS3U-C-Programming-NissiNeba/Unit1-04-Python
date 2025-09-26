265692e56947c4935dacac1e630b06a06e92b24b
Nissi Fobe Neba <nissi.fobe.neba@stu.ocsb.ca>
2025-09-26 18:01:42 +0000

    added basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Created By: nissi
+# Date: 25/09/2025
+# Write what this program does ;it does basic math
+
+
+def main():
+    print("8+4={}".format(8 + 4))
+    print("8-4={}".format(8 - 4))
+    print("8*4={}".format(8 * 4))
+    print("8/4={}".format(8 * 4))
+    print("4*0.5={}".format(4 * 0.5))
+    print("4**8={}".format(4**8))
+
+
+if __name__ == "__main__":
+    main()
